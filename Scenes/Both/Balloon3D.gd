50267ad3ef7268fedf5b7b6ef54cfe798ff02b1d
Luxem <luxem192@gmail.com>
2021-12-15 01:26:29 +0100

    Push de truc pas fini

--- Scenes/Both/Balloon3D.gd
+++ Scenes/Both/Balloon3D.gd
@@ -40,6 +40,7 @@ func _on_HitboxBallon_area_shape_entered(area_id, area, area_shape, local_shape)
 		print("aie balloon")
 #		take_damage(1)
 		Network.CallPopBalloon(path,id,10)
+		get_parent().queue_free()
 		
 
 func take_damage(damage):
